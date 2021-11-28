cec8967ff0a5c3a634def8e87b3a6ef9c28163d7
Emil Sadekin Islam <45220779+nilemil007@users.noreply.github.com>
2021-11-29 00:22:21 +0600

    add database login credentials

--- database.php
+++ database.php
@@ -1,5 +1,8 @@
 <?php
     // Create database class
     class Database{
-
+        private $host = 'localhost';
+        private $user = 'root';
+        private $pass = '';
+        private $dbname = 'livechat';
     }
